edb9e4e8da386a9336539acee698b1f5c99ef762
TeckFirst <support@teckfirst.com>
2026-06-04 18:20:18 -0600

    Started putting in comments for how the RegionConfig class will work.

--- Assets/Scripts/RegionConfig.cs
+++ Assets/Scripts/RegionConfig.cs
@@ -3,5 +3,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "RegionConfig", menuName = "Scriptable Objects/RegionConfig")]
 public class RegionConfig : ScriptableObject
 {
-    
+    // Define ore drop rates for different regions
+
+    // Define starting obstacle chances for different regions
+
+    // Define the amount of ore needed to collect before moving to the next region
+
+
 }
